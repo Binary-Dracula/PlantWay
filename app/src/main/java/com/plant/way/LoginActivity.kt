d783cef3260e78df26fbf79418cbdcdc84a9fa6c
summer <summer.work.001@gmail.com>
2025-11-05 23:19:06 +0800

    login画面

---
+++ app/src/main/java/com/plant/way/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.plant.way
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        findViewById<View?>(R.id.btn_login)?.setOnClickListener { v: View? ->
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
