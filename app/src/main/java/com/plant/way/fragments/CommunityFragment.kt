b40c94689bc72f92e1b5ab287de6569fda66ff0a
summer <summer.work.001@gmail.com>
2025-11-06 20:29:22 +0800

    Community画面

--- app/src/main/java/com/plant/way/fragments/CommunityFragment.kt
+++ app/src/main/java/com/plant/way/fragments/CommunityFragment.kt
@@ -4,7 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.plant.way.R
 
@@ -15,12 +16,20 @@ class CommunityFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = TextView(requireContext()).apply {
-            text = "Community Fragment"
-            textSize = 24f
-            gravity = android.view.Gravity.CENTER
+        return inflater.inflate(R.layout.fragment_community, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        setupClickListeners(view)
+    }
+
+    private fun setupClickListeners(view: View) {
+        // Add button click listener
+        view.findViewById<ImageView>(R.id.iv_add).setOnClickListener {
+            Toast.makeText(requireContext(), "Add new item", Toast.LENGTH_SHORT).show()
         }
-        return view
     }
 
     companion object {
